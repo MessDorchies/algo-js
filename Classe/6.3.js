8da4840a4907720f96fd787dc8d59891f420cfa9
Mess Dorchies <messaouddorchies@gmail.com>
2021-09-10 14:38:59 +0200

    commitnew

---
+++ Classe/6.3.js
@@ -0,0 +1,43 @@
+class Rectangle{
+
+    constructor(topLeftXPos, topLeftYPos, width, length){
+        
+        this.topLeftXPos = topLeftXPos;
+        this.topLeftYPos = topLeftYPos;
+        this.width = width;
+        this.length = length;
+    }
+
+    collide(otherReclangle = new Rectangle()){
+
+        if((this.topLeftXPos + this.length) === otherReclangle.topLeftXPos || (this.topLeftYPos + this.width) === otherReclangle.topLeftYPos || this.topLeftXPos === (otherReclangle.topLeftXPos + otherReclangle.length) || this.topLeftYPos === (otherReclangle.topLeftYPos + otherReclangle.width)){
+            return true;
+        }
+        else{
+            return false;
+        } 
+    }
+}
+
+function generateAndDisplay(){
+
+    let allRectangle = [];
+    let allCollide = [];
+    for(i = 0; i<=1000; i++){
+
+        allRectangle[i] = new Rectangle(Math.floor(Math.random()*100),Math.floor(Math.random()*100),Math.floor(Math.random()*100),Math.floor(Math.random()*100));
+    }
+    for(j = 0; j <= 1000; j++){
+        let k = 0;
+        while(allRectangle[j].collide(allRectangle[k]) === false){
+            k++;
+            if(allRectangle[j].collide(allRectangle[k])){
+                allCollide.push(allRectangle[j]);
+            }
+        }
+        
+    }
+    return allCollide;
+}
+
+console.log(generateAndDisplay());
